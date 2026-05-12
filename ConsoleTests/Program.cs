e48660a2b3bb94f64670e57ecaea1c67a9da0e36
Silvainius <connor.adam@ymail.com>
2026-05-12 14:08:37 -0700

    Moved to new repo

---
+++ ConsoleTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandEngine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
